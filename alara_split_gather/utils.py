34f34821c2519e8b3b8d1b8d90af4a05db005d72
zxkjack123 <zxkjack123@163.com>
2021-04-06 22:07:49 +0800

    rename calc_subtask_ids to assign_subtasks

--- alara_split_gather/utils.py
+++ alara_split_gather/utils.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-def calc_subtask_ids(total_num, num_tasks=2):
+def assign_subtasks(total_num, num_tasks=2):
     """
     Split the task into sub-tasks.
 
@@ -17,15 +17,15 @@ def calc_subtask_ids(total_num, num_tasks=2):
         List of the list of ids. Eg.
         [[0, 1], [2, 3, 4]]
     """
-    subtask_ids = []
+    subtasks = []
     num_per_task = total_num // num_tasks
     for i in range(num_tasks):
         if i < num_tasks-1:
             ids = list(range(i*num_per_task, (i+1)*num_per_task, 1))
         else:
             ids = list(range(i*num_per_task, total_num))
-        subtask_ids.append(ids)
-    return subtask_ids
+        subtasks.append(ids)
+    return subtasks
         
         
 
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -9,22 +9,22 @@ from numpy.testing import assert_array_equal, assert_array_almost_equal
 
 import numpy as np
 import filecmp
-from alara_split_gather.utils import calc_subtask_ids
+from alara_split_gather.utils import assign_subtasks
 
 cwd = os.getcwd()
 
-def test_calc_subtask_ids():
+def test_assign_subtasks():
     # 
     total_num = 4
     num_tasks = 2
-    exp_subtask_ids = [[0, 1], [2, 3]]
-    subtask_ids = calc_subtask_ids(total_num, num_tasks=num_tasks)
-    assert_array_equal(subtask_ids, exp_subtask_ids)
+    exp_subtasks = [[0, 1], [2, 3]]
+    subtasks = assign_subtasks(total_num, num_tasks=num_tasks)
+    assert_array_equal(subtasks, exp_subtasks)
     #  
     total_num = 5
     num_tasks = 2
-    exp_subtask_ids = [[0, 1], [2, 3, 4]]
-    subtask_ids = calc_subtask_ids(total_num, num_tasks=num_tasks)
-    assert_array_equal(subtask_ids, exp_subtask_ids)
+    exp_subtasks = [[0, 1], [2, 3, 4]]
+    subtasks = assign_subtasks(total_num, num_tasks=num_tasks)
+    assert_array_equal(subtasks, exp_subtasks)
 
 
