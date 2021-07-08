704022ababff056ad6459e4b1b08d82b05ee7afe
zxkjack123 <zxkjack123@163.com>
2021-07-08 11:33:26 +0800

    update version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
  
 setup(name='alara_split_gather',
-      version='0.1.1',
+      version='0.1.2',
       url='https://github.com/zxkjack123/alara_split_gather',
       license='MIT',
       author='Xiaokang Zhang',
